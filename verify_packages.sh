ac90ac912791c0ef7cb45c93430fb321347bdeaf
Chris Lyons <chris@lyonsgroup.family>
2021-09-27 11:22:55 -0400

    updating for local resources

--- verify_packages.sh
+++ verify_packages.sh
@@ -3,7 +3,7 @@
 while IFS="" read -r p || [ -n "$p" ]; do
   rpm_validate=`$p | tr -d '.x86_64'`
   echo "checking for rpm -> $rpm_validate"
-  if [ ! -f "$rpm_validate".* ]; then
+  if [ ! -z `ls -al $2 | grep $rpm_validate` ]; then
     echo "RPM NOT FOUND -> $p"
   fi
 done < $1
